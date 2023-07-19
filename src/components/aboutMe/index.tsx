5c7e172b874fe0868f11a8af44e0edc7b2d6ed51
norvalbv <benjinorval@gmail.com>
2023-07-19 22:56:21 +0100

    update contactMe

--- src/components/aboutMe/index.tsx
+++ src/components/aboutMe/index.tsx
@@ -1,20 +1,23 @@
-import "./about-me.scss";
 import profilePhoto from "../../files/images/profile-photo.png";
 
 const AboutMe = () => {
   return (
-    <div id="about">
-      <div className="about-image">
+    <div className="flex items-center h-screen -mt-12 md:flex-col-reverse md:justify-start md:h-auto">
+      <div className="w-full grid grid-cols-16 grid-rows-16 md:w-auto md:inline-block">
         <img
           src={profilePhoto}
           alt="Headshot of Benjamin Norval"
-          id="overlayImage"
+          className="z-10 w-7/10 grid-row-start-1 grid-row-end-17 grid-col-start-5 grid-col-end-17 shadow-md md:w-3/10 md:shadow-none md:block md:rounded-xl md:m-auto"
         />
-        <img src={profilePhoto} alt="" id="underlayImage" />
+        <img
+          src={profilePhoto}
+          alt=""
+          className="w-13/20 opacity-40 filter grayscale grid-row-start-2 grid-row-end-17 grid-col-start-4 grid-col-end-17 shadow-md md:hidden"
+        />
       </div>
-      <div className="about-text">
-        <h2>About Me</h2>
-        <p>
+      <div className="text-center w-full md:mt-12">
+        <h2 className="text-4xl font-semibold md:text-5xl">About Me</h2>
+        <p className="my-6 mx-auto w-7/10 text-lg text-primary-light md:text-xl">
           Business graduate turned Full Stack software developer that has 1.5
           years of experience in web development.
           <br />
@@ -26,6 +29,7 @@ const AboutMe = () => {
           <br />I would love for you to check out my projects and even reach
           out!
         </p>
+        <button className="h-10 border-none rounded-xl cursor-pointer w-32 text-white bg-secondary-1">View Projects</button>
       </div>
     </div>
   );
--- src/components/contactMe/index.tsx
+++ src/components/contactMe/index.tsx
@@ -1,75 +1,75 @@
-import "./contact-me.scss";
-
 const ContactMe = () => {
   return (
-    <div id="contact">
-      <h2 id="contact-title">Come Say Hi!</h2>
-      <p id="contact-text">
-        Drop me an email for any reason or alternatively, reach out via my
-        socials
-      </p>
+    <div className="flex flex-col items-center gap-8 p-16 bg-gradient-to-b from-transparent to-primary-dark">
+      <h2 className="text-4xl font-semibold md:text-5xl">Come Say Hi!</h2>
+      <p className="text-lg text-primary-light md:text-xl">Drop me an email for any reason or alternatively, reach out via my socials</p>
       <form
         action="https://formsubmit.co/benjinorval@gmail.com"
         method="POST"
-        className="input-container"
+        className="w-1/3 md:w-7/10 sm:w-full"
       >
-        <h3 className="input-label">Name</h3>
-        <div className="input">
+        <h3 className="text-secondary-1 capitalize tracking-wide text-sm">Name</h3>
+        <div className="relative mb-4">
           <input
             type="text"
             minLength="1"
             maxLength="100"
             pattern="[a-zA-Z]+"
             required
-            className="input-field"
+            className="w-full h-9 mb-2 bg-transparent border-none px-2 text-primary-light outline-none"
             placeholder="Name"
           />
+          <div className="absolute bottom-2 left-0 w-full h-1 bg-gradient-to-r from-secondary-1 to-secondary-2"></div>
         </div>
-        <h3 className="input-label">Email</h3>
-        <div className="input">
+        <h3 className="text-secondary-1 capitalize tracking-wide text-sm">Email</h3>
+        <div className="relative mb-4">
           <input
             type="text"
             id="email"
-            className="input-field"
             required
+            className="w-full h-9 mb-2 bg-transparent border-none px-2 text-primary-light outline-none"
             placeholder="Email"
           />
+          <div className="absolute bottom-2 left-0 w-full h-1 bg-gradient-to-r from-secondary-1 to-secondary-2"></div>
         </div>
-        <h3 className="input-label">Send me a message</h3>
-        <div className="input">
+        <h3 className="text-secondary-1 capitalize tracking-wide text-sm">Send me a message</h3>
+        <div className="relative mb-4">
           <input
             type="text"
             name="_subject"
+            className="w-full h-9 mb-2 bg-transparent border-none px-2 text-primary-light outline-none"
             placeholder="Subject"
-            className="input-field"
           />
+          <div className="absolute bottom-2 left-0 w-full h-1 bg-gradient-to-r from-secondary-1 to-secondary-2"></div>
         </div>
-        <div className="input">
+        <div className="relative mb-4">
           <textarea
-            placeholder="Send me a message!"
-            className="input-field"
-            style={{ height: "10rem" }}
             required
-          />
+            className="w-full h-40 bg-transparent border-none px-2 text-primary-light outline-none resize-y"
+            placeholder="Send me a message!"
+          ></textarea>
+          <div className="absolute bottom-2 left-0 w-full h-1 bg-gradient-to-r from-secondary-1 to-secondary-2"></div>
         </div>
-        <input type="submit" id="submit" />
+        <input type="submit" className="w-20 h-8 mb-1 bg-primary-light border-2 border-transparent border-gradient-to-r from-secondary-1 to-secondary-2 cursor-pointer" />
       </form>
-      <p style={{ fontSize: "14px" }}>Reach me on my socials...</p>
-      <ul id="socials">
+      <p className="text-sm">Reach me on my socials...</p>
+      <ul className="flex justify-center gap-4">
         <li>
           <a
             href="https://github.com/norvalbv/"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-base uppercase font-light hover:underline hover:opacity-50 cursor-pointer"
           >
             GitHub
           </a>
         </li>
         <li>
           <a
-            href="https://www.linkedin.com/in/benjamin-norval-a36ab01b1/"
+            href="https://www.linkedin.com/in/benjamin-norval/"
             target="_blank"
             rel="noopener noreferrer"
+            className="text-base uppercase font-light hover:underline hover:opacity-50 cursor-pointer"
           >
             Linked In
           </a>
