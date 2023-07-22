4c5ba797550907702c3f9b6b41dc75e368b9d7ae
norvalbv <benjinorval@gmail.com>
2023-07-22 17:42:41 +0100

    update contact form

--- src/components/ContactDetails/index.tsx
+++ src/components/ContactDetails/index.tsx
@@ -7,51 +7,50 @@ import {
   PHONE_NUMBER_COUNTRY_CODE,
 } from 'constants/index';
 
+const contactDetails = [
+  {
+    id: 'email',
+    key: 'Email',
+    hrefLink: `mailto:${CONTACT_EMAIL}`,
+    label: CONTACT_EMAIL,
+  },
+  {
+    id: 'phone',
+    key: 'Phone',
+    hrefLink: `tel:${PHONE_NUMBER_COUNTRY_CODE}${PHONE_NUMBER}`,
+    label: `(${PHONE_NUMBER_COUNTRY_CODE}) ${PHONE_NUMBER}`,
+  },
+  {
+    id: 'linkedin',
+    key: 'Linked In',
+    hrefLink: LINKED_IN,
+    label: 'Benjamin Norval',
+  },
+  {
+    id: 'github',
+    key: 'GitHub',
+    hrefLink: GITHUB,
+    label: 'norvalbv',
+  },
+];
+
 const ContactDetails = (): ReactElement => {
   return (
-    <div className="flex flex-col gap-2">
-      <h2 className="mb-6">Contact Details:</h2>
+    <div className="flex flex-col gap-2 rounded-lg bg-white/70 p-6 dark:bg-dark-dark/30 lg:w-2/5">
+      <h2 className="mb-6 underline underline-offset-8">Contact Details:</h2>
       <p>If you would like to contact me directly;</p>
-      <span>
-        Email:&nbsp;
-        <a
-          rel="noreferrer"
-          href={`mailto:${CONTACT_EMAIL}`}
-          className="underline transition duration-150 ease-linear hover:text-accent-secondary"
-        >
-          {CONTACT_EMAIL}
-        </a>
-      </span>
-      <span>
-        Phone:&nbsp;
-        <a
-          rel="noreferrer"
-          href={`tel:${PHONE_NUMBER_COUNTRY_CODE}${PHONE_NUMBER}`}
-          className="underline transition duration-150 ease-linear hover:text-accent-secondary"
-        >
-          ({PHONE_NUMBER_COUNTRY_CODE})&nbsp;{PHONE_NUMBER}
-        </a>
-      </span>
-      <span>
-        Linked In:&nbsp;
-        <a
-          rel="noreferrer"
-          href={LINKED_IN}
-          className="underline transition duration-150 ease-linear hover:text-accent-secondary"
-        >
-          Benjamin Norval
-        </a>
-      </span>
-      <span>
-        GitHub:&nbsp;
-        <a
-          rel="noreferrer"
-          href={GITHUB}
-          className="underline transition duration-150 ease-linear hover:text-accent-secondary"
-        >
-          norvalbv
-        </a>
-      </span>
+      {contactDetails.map((contact) => (
+        <span className="my-2" key={contact.id}>
+          {contact.key}:&nbsp;
+          <a
+            rel="noreferrer"
+            href={contact.hrefLink}
+            className="my-4 underline transition duration-150 ease-linear hover:text-accent-secondary"
+          >
+            {contact.label}
+          </a>
+        </span>
+      ))}
     </div>
   );
 };
--- src/components/contactMe/index.tsx
+++ src/components/contactMe/index.tsx
@@ -16,37 +16,39 @@ const ContactMe = (): ReactElement => {
 
   return (
     <CardWrapper title="Come Say Hi!">
-      <form
-        action="https://formsubmit.co/benjinorval@gmail.com"
-        method="POST"
-        className="rounded-lg bg-white/70 p-6 dark:bg-dark-dark/30 lg:w-3/5"
-      >
-        <h2 className="mb-6">Contact Form:</h2>
-        <TextField
-          label="Name"
-          value={query.name}
-          onChange={(val): void => setQueryByKey('name', val)}
-          required
-        />
-        <TextField
-          label="Email"
-          value={query.email}
-          onChange={(val): void => setQueryByKey('email', val)}
-          required
-          className="my-6"
-        />
-        <TextField
-          label="Message"
-          value={query.message}
-          onChange={(val): void => setQueryByKey('message', val)}
-          inputType="textarea"
-          required
-        />
-        <button type="submit" className="mt-6 hover:underline">
-          Submit
-        </button>
-      </form>
-      <ContactDetails />
+      <div className="flex flex-col gap-6 lg:flex-row">
+        <form
+          action="https://formsubmit.co/benjinorval@gmail.com"
+          method="POST"
+          className="rounded-lg bg-white/70 p-6 dark:bg-dark-dark/30 lg:w-3/5"
+        >
+          <h2 className="mb-6 underline underline-offset-8">Contact Form:</h2>
+          <TextField
+            label="Name"
+            value={query.name}
+            onChange={(val): void => setQueryByKey('name', val)}
+            required
+          />
+          <TextField
+            label="Email"
+            value={query.email}
+            onChange={(val): void => setQueryByKey('email', val)}
+            required
+            className="my-6"
+          />
+          <TextField
+            label="Message"
+            value={query.message}
+            onChange={(val): void => setQueryByKey('message', val)}
+            inputType="textarea"
+            required
+          />
+          <button type="submit" className="mt-6 hover:underline">
+            Submit
+          </button>
+        </form>
+        <ContactDetails />
+      </div>
     </CardWrapper>
   );
 };
