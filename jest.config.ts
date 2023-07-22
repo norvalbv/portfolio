7e97e6e15a7393f7b9511b26bbb320befd0c9a4b
norvalbv <benjinorval@gmail.com>
2023-07-22 23:19:03 +0100

    add jest config and dotenv-flow

---
+++ jest.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'jest';
+import * as dotenv from 'dotenv-flow';
+
+dotenv.config();
+
+export default (): Config => {
+  return {
+    clearMocks: true,
+    collectCoverageFrom: [
+      '<rootDir>/src/**/*.{ts,tsx,js,jsx}',
+      '!<rootDir>/src/**/*.d.ts',
+      '!<rootDir>/src/routes/**',
+    ],
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+    preset: 'ts-jest',
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    watchPathIgnorePatterns: ['<rootDir>/src/cypress/'],
+    moduleDirectories: ['node_modules', 'src'],
+  };
+};
