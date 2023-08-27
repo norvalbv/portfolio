78023d6129bdb0b1fc28a2ca1023e2e5329d71c6
norvalbv <benjinorval@gmail.com>
2023-08-27 13:12:45 +0100

    add front Matter to blog

---
+++ src/components/Badge/index.tsx
@@ -0,0 +1,11 @@
+import React, { ReactElement } from 'react';
+
+type BadgeProps = {
+  tag: string;
+};
+
+const Badge = ({ tag }: BadgeProps): ReactElement => {
+  return <div className="text-x m-2 w-max rounded-lg bg-accent-secondary px-4 py-0.5">{tag}</div>;
+};
+
+export default Badge;
--- src/pages/Blogs/Blog/index.tsx
+++ src/pages/Blogs/Blog/index.tsx
@@ -6,7 +6,61 @@ import Loader from 'components/Loader';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { arta } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import useTheme from 'hooks/useTheme';
+import Badge from 'components/Badge';
 import { blogs } from '..';
+
+type FrontMatter = {
+  Aliases: string[];
+  'date created': string | null;
+  'date modified': string | null;
+  'review-frequency': string | null;
+  tags: string[] | null;
+  title: string | null;
+} & Record<string, string | string[]>;
+
+const parseFrontMatterToArray = (lines: string[]): FrontMatter => {
+  const obj: FrontMatter = {
+    Aliases: [],
+    'date created': null,
+    'date modified': null,
+    'review-frequency': null,
+    tags: [],
+    title: null,
+  };
+
+  let currentKey: null | string = null;
+
+  lines.forEach((line) => {
+    if (line.startsWith('---')) {
+      // Skip the delimiter lines
+      return;
+    }
+
+    const splitLine = line.split(':');
+
+    if (splitLine.length > 1) {
+      currentKey = splitLine[0].trim();
+      const value = splitLine.slice(1).join(':').trim();
+
+      if (value.startsWith('[')) {
+        // Initialize an array for later
+        obj[currentKey] = [];
+      } else if (value) {
+        obj[currentKey] = value;
+      } else {
+        // Initialize as an empty array if value is not present
+        obj[currentKey] = [];
+      }
+    } else if (currentKey && Array.isArray(obj[currentKey])) {
+      const listItem = line.trim().replace(/^-/, '').trim();
+      if (listItem) {
+        (obj[currentKey] as string[]).push(listItem);
+      }
+    }
+  });
+
+  return obj;
+};
 
 const UnorderedListComponent = ({
   children,
@@ -45,6 +99,7 @@ const CodeComponent = ({ children, ...props }: { children: string }): ReactEleme
 
 const Blog = (): ReactElement => {
   const [blog, setBlog] = useState<string | null>(null);
+  const [frontMatter, setFrontMatter] = useState<FrontMatter | null>(null);
   const { pathname } = useLocation();
 
   const currentBlogIndex = blogs.findIndex((b) => pathname.includes(b.url));
@@ -58,22 +113,63 @@ const Blog = (): ReactElement => {
       .then((res: { default: RequestInfo }) =>
         fetch(res.default)
           .then((res) => res.text())
-          .then((res) => setBlog(res))
+          .then((res) => {
+            const extractFrontMatter = (markdownString: string): void => {
+              const lines = markdownString.split('\n');
+
+              // Due to the syntax of the markdown, there will always be three dashed lines in front matter.
+              const frontMatterIndexes = lines.flatMap((line, i) => (line === '---' ? i : []));
+
+              const frontMatter = lines.slice(0, frontMatterIndexes[2] + 1);
+
+              const blog = lines.slice(frontMatterIndexes[2] + 1).join(' ');
+
+              setFrontMatter(parseFrontMatterToArray(frontMatter));
+            };
+            extractFrontMatter(res);
+            setBlog(res.slice(288));
+          })
       )
       .catch(() => {});
   }, [currentBlogIndex, file]);
 
   if (!blog) return <Loader />;
 
+  // TODO filter out all [[]] link tags before putting it into blog, add to a state and update them to create anchor tags instead.
+
   return (
     <CardWrapper className="w-7/12">
       <article>
         <h1 className="text-2xl capitalize text-accent-main underline">{title}</h1>
+        <section className="my-6 flex flex-col gap-2 text-xs">
+          {frontMatter?.tags?.length ? (
+            <div className="flex items-center gap-2">
+              Tags:
+              {frontMatter?.tags.map((tag) => <Badge tag={tag} key={tag} />)}
+            </div>
+          ) : null}
+          {frontMatter?.['date modified'] && (
+            <p>
+              Last Modified: <span className="italic">{frontMatter['date modified']}</span>
+            </p>
+          )}
+          {frontMatter?.Aliases?.length ? (
+            <div className="flex items-center gap-2">
+              Aliases:
+              {frontMatter?.Aliases.map((tag, i, a) => (
+                <div key={tag} className="flex items-center gap-2">
+                  {tag}
+                  {i < a.length - 1 && ','}
+                </div>
+              ))}
+            </div>
+          ) : null}
+        </section>
         <Markdown
           options={{
             wrapper: React.Fragment,
             overrides: {
-              h1: { props: { className: 'my-10 text-2xl text-accent-main' } },
+              h1: { props: { className: 'text-2xl text-accent-main' } },
               h2: { props: { className: 'my-6 text-2xl underline text-accent-main/90' } },
               h3: { props: { className: 'my-4 text-lg text-accent-main/75' } },
               p: { props: { className: 'my-3 text-sm leading-6' } },
