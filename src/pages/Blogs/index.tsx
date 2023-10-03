900146eda9816e6a5fbece9afeefb63db929e8ef
norvalbv <benjinorval@gmail.com>
2023-10-03 16:51:04 +0100

    fix typing

--- src/pages/Blogs/index.tsx
+++ src/pages/Blogs/index.tsx
@@ -1,15 +1,19 @@
+import Header from 'components/Header';
 import React, { ReactElement } from 'react';
 import Blog, { DefBlogs } from 'react-blogs';
+import { useSearchParams } from 'react-router-dom';
 
 const Blogs = (): ReactElement => {
+  const [searchParams] = useSearchParams();
+
   const allBlogs: DefBlogs = {
     style: '',
     blogs: [
       {
         id: 'how-memory-works',
-        file: '../../assets/blogs/memory.md',
-        title: 'How Memory (RAM) Works',
-        subtitle: 'A deep insight to what RAM is and how it interacts with the computer.',
+        file: '/src/assets/blogs/memory.md',
+        title: { text: 'How Memory (RAM) Works' },
+        subtitle: { text: 'A deep insight to what RAM is and how it interacts with the computer.' },
         url: 'how-memory-works',
         containsFrontMatter: true,
         frontMatterPosition: 'top',
@@ -21,10 +25,10 @@ const Blogs = (): ReactElement => {
       },
       {
         id: 'memory',
-        file: '../../assets/blogs/memory-heap.md',
-        title: 'How Memory (RAM) Wo22rks',
-        subtitle: 'A deep insight to what RAM is and how it interacts with the computer.',
+        file: '/src/assets/blogs/memory.md',
+        title: { text: 'How Memory (RAM) Wo1232rks' },
         url: 'how-memory-works',
+        containsFrontMatter: true,
         metadata: {
           'read time': 20,
           level: 'Beginner Friendly',
@@ -35,8 +39,9 @@ const Blogs = (): ReactElement => {
   };
 
   return (
-    <div className="mx-auto w-8/12">
-      <Blog allBlogs={allBlogs} paramKey="hi" theme="LIGHT_THEME" />
+    <div className="mx-auto w-8/12 font-mono text-2xl">
+      {!searchParams.get('blog') && <Header title="Benji's Blogs" />}
+      <Blog allBlogs={allBlogs} paramKey="blog" theme="DARK_THEME" />
     </div>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
-import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html';
 import EnvironmentPlugin from 'vite-plugin-environment';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown';
 
 export default defineConfig({
   plugins: [
     react({ include: /\.(ts|tsx)$/ }),
+    mdPlugin({ mode: [Mode.HTML, Mode.REACT] }),
     viteTsconfigPaths(),
     EnvironmentPlugin('all'),
     createHtmlPlugin({
       minify: true,
     }),
   ],
-  assetsInclude: ['**/*.md'],
+  assetsInclude: [/\.md$/, '**/*.md'],
   ...(process.env.NODE_ENV === 'development'
     ? {
         define: {
