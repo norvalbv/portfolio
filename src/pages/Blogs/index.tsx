a560676029dfee87e2e149720739fd1ebbc4888b
norvalbv <benjinorval@gmail.com>
2023-10-06 17:26:22 +0100

    fix typing

--- src/pages/Blogs/index.tsx
+++ src/pages/Blogs/index.tsx
@@ -6,38 +6,35 @@ import { useSearchParams } from 'react-router-dom';
 const Blogs = (): ReactElement => {
   const [searchParams] = useSearchParams();
 
-  const allBlogs: DefBlogs = {
-    style: '',
-    blogs: [
-      {
-        id: 'how-memory-works',
-        file: '/src/assets/blogs/memory.md',
-        title: { text: 'How Memory (RAM) Works' },
-        subtitle: { text: 'A deep insight to what RAM is and how it interacts with the computer.' },
-        url: 'how-memory-works',
-        showFrontMatter: true,
-        metadata: {
-          'read time': '20 minutes',
-          level: 'Beginner Friendly',
-          'date posted': 'Monday, 28th August 2023',
-        },
+  const allBlogs: DefBlogs = [
+    {
+      id: 'how-memory-works',
+      file: '/src/assets/blogs/memory.md',
+      title: { text: 'How Memory (RAM) Works' },
+      subtitle: { text: 'A deep insight to what RAM is and how it interacts with the computer.' },
+      url: 'how-memory-works',
+      showFrontMatter: true,
+      metadata: {
+        'read time': '20 minutes',
+        level: 'Beginner Friendly',
+        'date posted': 'Monday, 28th August 2023',
       },
-      {
-        id: 'Memory Heap',
-        file: '/src/assets/blogs/memory-heap.md',
-        title: { text: 'What is memory heap?' },
-        url: 'memory-heap',
-        showFrontMatter: true,
-        metadata: {
-          'read time': '6 minutes',
-          level: 'Intermediate',
-          'date posted': 'Monday, 29th August 2023',
-        },
+    },
+    {
+      id: 'Memory Heap',
+      file: '/src/assets/blogs/memory-heap.md',
+      title: { text: 'What is memory heap?' },
+      url: 'memory-heap',
+      showFrontMatter: true,
+      metadata: {
+        'read time': '6 minutes',
+        level: 'Intermediate',
+        'date posted': 'Monday, 29th August 2023',
       },
-    ],
-  };
+    },
+  ];
 
-  allBlogs.blogs.sort((a, b) => {
+  allBlogs.sort((a, b) => {
     const regex = /(?<=\d)(?:st|nd|rd|th)\b/i;
 
     const metaA = a.metadata?.['date posted'].replace(regex, '');
@@ -52,7 +49,7 @@ const Blogs = (): ReactElement => {
   return (
     <div className="mx-auto w-8/12 font-mono text-2xl">
       {!searchParams.get('blog') && <Header title="Benji's Blogs" />}
-      <Blog allBlogs={allBlogs} paramKey="blog" theme="DARK_THEME" />
+      <Blog allBlogs={allBlogs} style="github" />
     </div>
   );
 };
