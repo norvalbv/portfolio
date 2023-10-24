8bb4674409422574b2d64785615b208ef4729b0d
norvalbv <benjinorval@gmail.com>
2023-10-24 22:17:54 +0100

    fix

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -5,7 +5,7 @@ const ContactMe = (): ReactElement => {
   return (
     <section className="mt-6">
       Liking this blog?&nbsp;
-      <Link to="/contact-me">
+      <Link to="/contact">
         <button type="button" className="rounded-lg bg-accent-secondary px-3">
           Give me a message!
         </button>
