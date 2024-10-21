a826b9f4211833ed90ce6e00f3b6dd56b358fcf5
norvalbv <benjinorval@gmail.com>
2024-10-21 19:31:13 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@smithy/util-retry'],
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    });
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/blogs/:path*',
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+      },
+    ];
   },
 };
 
