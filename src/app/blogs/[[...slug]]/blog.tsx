b8f00cb62ce5dfc75fd272cb0a6b5d34399b7263
norvalbv <benjinorval@gmail.com>
2024-10-25 22:10:30 +0100

    test

--- src/app/blogs/[[...slug]]/blog.tsx
+++ src/app/blogs/[[...slug]]/blog.tsx
@@ -151,7 +151,7 @@ const fetchBlogContent = async (blog?: string, type?: 'notes' | 'blog'): Promise
       content = (await getS3ObjectByUrl(blog)) || '';
     } else {
       console.log(BASE_URL, blog);
-      const response = await fetch(`${BASE_URL}/${blog.replace(/^\/+/, '')}`);
+      const response = await fetch(`${BASE_URL}/${blog}`);
       content = await response.text();
     }
 
@@ -160,8 +160,8 @@ const fetchBlogContent = async (blog?: string, type?: 'notes' | 'blog'): Promise
       return null;
     }
 
-    const contentWithImages = await replaceImageReferences(content);
-    return filterContent(contentWithImages);
+    // const contentWithImages = await replaceImageReferences(content);
+    return filterContent(content);
   } catch (error) {
     console.error('Error fetching blog content:', error);
     return null;
