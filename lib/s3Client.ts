2d15ca980367c45bf715c23cb8e05422760b5e0c
norvalbv <benjinorval@gmail.com>
2024-10-26 12:35:44 +0100

    check vars

--- lib/s3Client.ts
+++ lib/s3Client.ts
@@ -1,8 +1,8 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
-const { S3_AWS_REGION, S3_AWS_ACCESS_KEY_ID, S3_AWS_SECRET_ACCESS_KEY } = process.env;
+const { S3_AWS_REGION, S3_AWS_ACCESS_KEY_ID, S3_AWS_SECRET_ACCESS_KEY, NEXT_PUBLIC_S3_BUCKET_NAME, EMAILJS_SERVICE_ID } = process.env;
 
-console.log(S3_AWS_REGION, S3_AWS_ACCESS_KEY_ID, S3_AWS_SECRET_ACCESS_KEY, 's3 credentials');
+console.log(S3_AWS_REGION, S3_AWS_ACCESS_KEY_ID, S3_AWS_SECRET_ACCESS_KEY, NEXT_PUBLIC_S3_BUCKET_NAME, EMAILJS_SERVICE_ID, 's3 credentials');
 
 if (!S3_AWS_REGION || !S3_AWS_ACCESS_KEY_ID || !S3_AWS_SECRET_ACCESS_KEY) {
   throw new Error('S3 credentials are not defined');
